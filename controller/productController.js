6a9f8a5104e07fe304fee3bbd2ba6f4308786ae5
Borhan Uddin <borhan.dev@gmail.com>
2022-02-24 17:15:58 +0600

    errorHandler Updated & Remove Bugs

--- controller/productController.js
+++ controller/productController.js
@@ -17,6 +17,6 @@ export const addProduct = async (req, res, next) => {
     const product = await Product.create(req.body);
     res.status(200).json({ success: true, product });
   } catch (err) {
-    return errorHandler(err.message, 400, res);
+    return errorHandler(err, 400, res);
   }
 };
--- utils/errorHandler.js
+++ utils/errorHandler.js
@@ -1,5 +1,10 @@
-const errorHandler = (message, statusCode, res) => {
-  res.status(statusCode).json({ success: false, message });
+const errorHandler = (err, statusCode, res) => {
+  if (err.code === 11000) {
+    err = `Duplicate ${Object.keys(err.keyValue)}:${Object.values(
+      err.keyValue
+    )} `;
+  }
+  res.status(statusCode).json({ success: false, message: err });
 };
 
 export default errorHandler;
