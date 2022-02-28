3d87b74d0e33ae8049281b1d99a6052b6996b3aa
Borhan Uddin <borhan.dev@gmail.com>
2022-02-28 09:53:09 +0600

    Fixed Bug

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -3,6 +3,7 @@ import {
   createUSer,
   getUser,
   loginUser,
+  logOut,
 } from "../controller/userController.js";
 import { verifyUser, isAuthenticatedUser } from "../middleware/auth.js";
 
@@ -11,15 +12,7 @@ const userRoutes = express.Router();
 userRoutes.post("/register", createUSer);
 userRoutes.post("/login", loginUser);
 userRoutes.get("/profile", isAuthenticatedUser, getUser);
-userRoutes.post("/logout", verifyUser, (req, res) => {
-  res
-    .cookie("token", null, {
-      expires: new Date(Date.now()),
-      httpOnly: true,
-    })
-    .status(200)
-    .json({ success: true, messgae: "Logout Succesfull" });
-});
+userRoutes.post("/logout", isAuthenticatedUser, logOut);
 userRoutes.post("/", verifyUser, (req, res) => {
   const user = req.user;
   res.status(200).json({ success: true, user });
--- utils/sendToken.js
+++ utils/sendToken.js
@@ -5,10 +5,11 @@ const sendToken = async (user, res, statusCode) => {
       Date.now() + process.env.COOKIE_EXPIRE * 24 * 60 * 60 * 1000
     ),
     httpOnly: true,
+    sameSite: true,
   };
   res
-    .cookie("token", token, options)
     .status(statusCode)
+    .cookie("token", token, options)
     .json({ success: true, token, user });
 };
 
